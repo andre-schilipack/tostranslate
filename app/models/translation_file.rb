452f02ba2390f70bea1d6523fc28611e6b010a11
Andre de Macedo Schilipack <andre.schilipack@gmail.com>
2015-04-10 02:57:26 -0300

    Small correction and percentage showing

--- app/models/translation_file.rb
+++ app/models/translation_file.rb
@@ -1,9 +1,13 @@
 class TranslationFile < ActiveRecord::Base
-  has_many :translation_files
+  has_many :translation_lines
 
   attr_accessor :file
 
   after_create :create_translation_lines
+
+  def percentage
+    self.translation_lines.where(translated: true).count.to_f / self.translation_lines.count.to_f * 100
+  end
 
   private
 
